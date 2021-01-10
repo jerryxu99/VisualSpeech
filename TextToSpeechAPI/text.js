f897bf1ca8aea3eca8d48a63d57476b84df3666b
Jerry Xu <jerry.xu99@gmail.com>
2021-01-09 21:28:31 -0800

    fixed TextToSpeech

--- TextToSpeechAPI/text.js
+++ TextToSpeechAPI/text.js
@@ -6,6 +6,7 @@ const fs = require('fs');
 const util = require('util');
 // Creates a client
 const client = new textToSpeech.TextToSpeechClient();
+
 async function quickStart() {
   // The text to synthesize
   const text = 'hello, world!';
@@ -22,8 +23,12 @@ async function quickStart() {
   // Performs the text-to-speech request
   const [response] = await client.synthesizeSpeech(request);
   // Write the binary audio content to a local file
-  const writeFile = util.promisify(fs.writeFile);
-  await writeFile('output.mp3', response.audioContent, 'binary');
-  console.log('Audio content written to file: output.mp3');
+
+  // const writeFile = util.promisify(fs.writeFile);
+  // await writeFile('output.mp3', response.audioContent, 'binary');
+  // console.log('Audio content written to file: output.mp3');
+  console.log(response.audioContent);
+  return response.audioContent;
 }
+
 quickStart();
