6e01be3be819ec2db08695ff3a716a67f75241ec
Jerry Xu <jerry.xu99@gmail.com>
2021-01-10 00:47:35 -0800

    put temp button to mimic new audio file functionality working

--- frontend/src/components/VisuallyImpaired.js
+++ frontend/src/components/VisuallyImpaired.js
@@ -1,7 +1,36 @@
+import { useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import ChatRoom from './ChatRoom';
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+//const getSpeech = require('../utils/text');
+
+const firestore = firebase.firestore();
 
 export default function VisuallyImpaired() {
+  useEffect(() => {
+    const doc = firestore.collection('audio').doc('buffer');
+    console.log('hello world');
+
+    const observer = doc.onSnapshot(
+      (docSnapshot) => {
+        console.log(`Received new audio`);
+        console.log(docSnapshot.data());
+      },
+      (err) => {
+        console.log(`Encountered error: ${err}`);
+      },
+    );
+  }, []);
+
+  const updateBuffer = async () => {
+    const audioRef = firestore.collection('audio').doc('buffer');
+
+    await audioRef.set({
+      buffer: 'world',
+    });
+  };
+
   return (
     <div className="App">
       <header className="App-header">
@@ -9,6 +38,7 @@ export default function VisuallyImpaired() {
           <Link to="/">Go Back</Link>
         </button>
         <h1>Visually Impaired</h1>
+        <button onClick={updateBuffer}>temp</button>
       </header>
 
       <section>
