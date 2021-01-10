19d321bf696a2522eb7f56121d0128b3678b5b23
Amanda Yu20 <amanda_yu2002@live.com>
2021-01-09 21:45:54 -0500

    added basic react template

---
+++ frontend/src/App.js
@@ -0,0 +1,21 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       
+        <p>
+          Text to Speech
+        </p>
+        <p>
+        Speech to Text 
+        </p>
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
